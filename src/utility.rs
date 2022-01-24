f605b767d9bf1564d267e5a4c37a5bf33f8c5a6b
LJNeon <23249107+LJNeon@users.noreply.github.com>
2022-01-23 21:07:27 -0700

    Fix formatting

--- src/utility.rs
+++ src/utility.rs
@@ -1,8 +1,8 @@
 use std::env::{var, VarError};
 
-use tracing::{subscriber, dispatcher::SetGlobalDefaultError};
-use tracing_subscriber::{fmt, layer::SubscriberExt};
+use tracing::{dispatcher::SetGlobalDefaultError, subscriber};
 use tracing_appender::{non_blocking, rolling};
+use tracing_subscriber::{fmt, layer::SubscriberExt};
 
 pub struct Env {
   pub discord_token: String,
@@ -26,9 +26,9 @@ impl Env {
 pub fn start_tracing() -> Result<non_blocking::WorkerGuard, SetGlobalDefaultError> {
   let (file_writer, guard) = non_blocking(rolling::daily("./logs", "log"));
 
-  subscriber::set_global_default(tracing_subscriber::fmt()
-    .finish()
-    .with(fmt::Layer::default().with_writer(file_writer)))?;
+  subscriber::set_global_default(
+    tracing_subscriber::fmt().finish().with(fmt::Layer::default().with_writer(file_writer))
+  )?;
 
   Ok(guard)
 }
